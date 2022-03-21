1fe479fd262b5964f027ec737aeb1225fb1d10c1
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-03-21 21:45:17 +0200

    refractor package.json scripts

--- packages/next-app/pages/_app.tsx
+++ packages/next-app/pages/_app.tsx
@@ -1,16 +1,53 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider, chain, defaultChains } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { WalletLinkConnector } from "wagmi/connectors/walletLink";
 import Heading from "../components/heading";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../theme";
+
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+
+const chains = defaultChains;
+
+type Connector =
+  | InjectedConnector
+  | WalletConnectConnector
+  | WalletLinkConnector;
+
+  const connectors = ({ chainId }: { chainId?: number }): Connector[] => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0];
+  return [
+    new InjectedConnector({ chains }),
+    new WalletConnectConnector({
+      options: {
+        infuraId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      options: {
+        appName: "Alpha-C",
+        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      },
+    }),
+  ];
+};
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <Provider autoConnect connectors={connectors}>
+      <ThemeProvider theme={theme}>
       <div>
         <Heading />
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
+    </Provider>
   );
 }
 
